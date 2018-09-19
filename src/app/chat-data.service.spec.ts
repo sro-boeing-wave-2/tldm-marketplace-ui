cbe0e9adac8ce3d76d80909c61970bc3305b3826
Deepak Acharya <aacharyadeepak100@gmail.com>
2018-09-19 15:15:47 +0530

    added some test cases

--- src/app/chat-data.service.spec.ts
+++ src/app/chat-data.service.spec.ts
@@ -1,15 +1,17 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { ChatDataService } from './chat-data.service';
+import { HttpModule } from '@angular/http';
 
 describe('ChatDataService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ChatDataService]
+      providers: [ChatDataService],
+      imports: [HttpModule]
     });
   });
 
-  // it('should be created', inject([ChatDataService], (service: ChatDataService) => {
-  //   expect(service).toBeTruthy();
-  // }));
+  it('should be created', inject([ChatDataService], (service: ChatDataService) => {
+    expect(service).toBeTruthy();
+  }));
 });
