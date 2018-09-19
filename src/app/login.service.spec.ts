1d49f5e82105ed32923b1d78a3d5f33afbb9c2f6
Deepak Acharya <aacharyadeepak100@gmail.com>
2018-09-19 18:27:41 +0530

    added one more test case

--- src/app/login.service.spec.ts
+++ src/app/login.service.spec.ts
@@ -2,17 +2,18 @@ import { TestBed, inject } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 
 import { LoginService } from './login.service';
+import { LocalStorageService } from 'ngx-webstorage';
 
 describe('LoginService', () => {
   let service: LoginService;
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [LoginService],
+      providers: [LoginService, LocalStorageService],
       imports: [HttpClientModule]
     });
   });
 
-  // it('should be created', inject([LoginService], (service: LoginService) => {
-  //   expect(service).toBeTruthy();
-  // }));
+  it('should be created', inject([LoginService], (service: LoginService) => {
+    expect(service).toBeTruthy();
+  }));
 });
