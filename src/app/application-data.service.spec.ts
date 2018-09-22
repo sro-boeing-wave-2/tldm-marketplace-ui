6c2c5ff8a7eb8fc187cc54a630252558ad1e7156
Deepak Acharya <aacharyadeepak100@gmail.com>
2018-09-22 07:58:49 +0530

    added more test cases to chat service

--- src/app/application-data.service.spec.ts
+++ src/app/application-data.service.spec.ts
@@ -1,15 +1,17 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { ApplicationDataService } from './application-data.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('ApplicationDataService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ApplicationDataService]
+      providers: [ApplicationDataService],
+      imports: [HttpClientModule]
     });
   });
 
-  // it('should be created', inject([ApplicationDataService], (service: ApplicationDataService) => {
-  //   expect(service).toBeTruthy();
-  // }));
+  it('should be created', inject([ApplicationDataService], (service: ApplicationDataService) => {
+    expect(service).toBeTruthy();
+  }));
 });
--- src/app/chat-data.service.spec.ts
+++ src/app/chat-data.service.spec.ts
@@ -2,16 +2,39 @@ import { TestBed, inject } from '@angular/core/testing';
 
 import { ChatDataService } from './chat-data.service';
 import { HttpClientModule } from '@angular/common/http';
+import { Channel } from './channel';
+import { User } from './user';
 
 describe('ChatDataService', () => {
+  let chatservice: ChatDataService;
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [ChatDataService],
       imports: [HttpClientModule]
     });
+    chatservice = TestBed.get(ChatDataService);
   });
 
   it('should be created', inject([ChatDataService], (service: ChatDataService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should be able to retrieve channels based on workspace and email', () => {
+    let channels: Channel [];
+    chatservice.getChannels('workspace','test@test.com').subscribe(data => {
+      expect(data).toBe(channels);
+    });
+  });
+
+  it('should be able to add bot to workspace', () => {
+    let botUser: User;
+    chatservice.addBotToWorkspace('workspacename',botUser).subscribe(data => {
+      expect().nothing;
+    })
+  });
+
+  it('should be able to add bot to channel', () => {
+    let botUser: User;
+    chatservice.addBot("channelId", botUser).subscribe(data => expect().nothing);
+  });
 });
