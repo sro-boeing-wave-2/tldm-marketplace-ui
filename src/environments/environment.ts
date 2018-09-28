d068d0f36097c3a2f117918f8754c30387dca896
Deepak Acharya <aacharyadeepak100@gmail.com>
2018-09-28 10:20:34 +0530

    changed chat api url in environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,11 +5,12 @@
 export const environment = {
   production: true,
   apiUrl: 'http://13.233.42.222:80/marketplace-api',
-  chatApiUrl: 'http://13.233.42.222:80/chat-api',
+  chatApiUrl: 'http://13.233.42.222:80/chat-api/api',
   onboardUrl: 'http://13.233.42.222:80/onboard-api',
   chatHubUrl: 'http://13.233.42.222:80/chat-api/chat',
   chatUIUrl: 'http://13.233.42.222:80/chat'
 };
+
 
 
 
