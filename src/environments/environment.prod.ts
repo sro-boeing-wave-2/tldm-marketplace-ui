72b527ebcdf2e4fb67b2303154a1690d850e98c5
Deepak Acharya <aacharyadeepak100@gmail.com>
2018-09-28 09:46:44 +0530

    chat backend url changed in environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'http://13.233.42.222:80/marketplace-api',
-  chatApiUrl: 'http://13.233.42.222:80/chat-api',
+  chatApiUrl: 'http://13.233.42.222:80/chat-api/api',
   onboardUrl: 'http://13.233.42.222:80/onboard-api',
   chatHubUrl: 'http://13.233.42.222:80/chat-api/chat',
   chatUIUrl: 'http://13.233.42.222:80/chat'
